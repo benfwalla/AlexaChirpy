00f60530c549d7d2768fcacad31c8334cdc762ee
Ben Wallace <benfwalla@gmail.com>
2017-12-02 17:22:52 -0500

    Update ChirpySpeechletRequestStreamHandler.java

--- src/main/java/ChirpySpeechletRequestStreamHandler.java
+++ src/main/java/ChirpySpeechletRequestStreamHandler.java
@@ -7,7 +7,7 @@ public class ChirpySpeechletRequestStreamHandler extends SpeechletRequestStreamH
     private static final Set<String> supportedAppIds = new HashSet<String>();
 
     static {
-        supportedAppIds.add("amzn1.ask.skill.38abff0b-4a96-4494-aaba-58ea6db6d9c4");
+        supportedAppIds.add("[Alexa App ID]");
     }
 
     public ChirpySpeechletRequestStreamHandler() {
